8bd664c4495fb72c5d7ca62acb79a338b6ea9b60
Oliver Tappe <zooey@hirschkaefer.de>
2025-02-22 11:25:09 +0100

    Adjust our idea of being online to include having a synchronized time.
    * As without this, go-librespot failed to accept the Spotify servers'
      TLS certificates, because of the pi0's time still not being set
      correctly.

--- yocto/sources/meta-hkaudio/recipes/iwd/files/wait-online.sh
+++ yocto/sources/meta-hkaudio/recipes/iwd/files/wait-online.sh
@@ -5,3 +5,7 @@ while ! ip route | grep -q default; do
 	sleep 1
 done
 
+while ! timedatectl | grep -q 'System clock synchronized: yes'; do
+	echo "waiting for time to be synchronized ..."
+	sleep 1
+done
