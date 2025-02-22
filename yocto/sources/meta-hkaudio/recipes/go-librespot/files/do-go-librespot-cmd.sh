0abe4cdc2851dcca3640de676bc997a894ee9fe2
Oliver Tappe <zooey@hirschkaefer.de>
2025-02-22 11:18:10 +0100

    Bring go-librespot into a working state.
    * Add support for our type of resource management to go-librespot.
    * Add support for different ALSA audio formats to go-librespot - this
      was required since the hifiberry-zeroamp doesn't seem to support
      the f32le format that go-librespot used to use by default. Let's use
      s16le instead.

---
+++ yocto/sources/meta-hkaudio/recipes/go-librespot/files/do-go-librespot-cmd.sh
@@ -0,0 +1,32 @@
+#! /bin/bash -e
+
+cmd=$1
+
+if [[ -z $cmd ]]; then
+    echo >&2 "$0: needs cmd, got: $@"
+    exit 5
+fi
+
+mkdir -p /run/go-librespot
+
+baseUrl=http://localhost:3678/player
+
+case "$cmd" in
+
+    pause-if-playing)
+        curl -4 -s -S -X POST ${baseUrl}/pause >/dev/null
+        ;;
+
+    play-pause)
+        curl -4 -s -S -X POST ${baseUrl}/playpause >/dev/null
+        ;;
+
+    volume-up)
+        curl -4 -s -S -X POST -d '{"volume":5,"relative":true}' ${baseUrl}/volume >/dev/null
+        ;;
+
+    volume-down)
+        curl -4 -s -S -X POST -d '{"volume":-5,"relative":true}' ${baseUrl}/volume >/dev/null
+        ;;
+
+esac
